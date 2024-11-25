4a5fe46443dbe4580c2c03bbb477c6a84bf06230
Hyunseung Ryu <rhseungg@gmail.com>
2024-11-25 23:14:57 +0900

    Initial commit

---
+++ src/main/kotlin/com/rhseung/thirsty/ThirstyDataGenerator.kt
@@ -0,0 +1,9 @@
+package com.rhseung.thirsty
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object ThirstyDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
